0eed693de29c4c6da55590150f23bac814f7bcf1
Babita00 <babitaasth07@gmail.com>
2024-05-24 18:15:29 +0545

    "Cloudinary added"

---
+++ src/utils/cloudinaryService.js
@@ -0,0 +1,30 @@
+
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        //upload the file in cloudinary
+        const response=await cloudinary.uploader.upload("localFilePath",
+        {
+            resource_type:"auto",
+            public_id: "olympic_flag" 
+        })
+            
+            console.log("File is uploaed in Cloudinary",response.url); 
+            return response;
+    }   
+
+    
+    catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temoporary file if operation gt failed
+    }
+}
+
+export {uploadOnCloudinary}
